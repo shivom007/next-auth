2f49a94d95cb659b92652af24c4ed0468e9ee886
shivom007 <shivmahto37@gmail.com>
2024-04-22 17:35:31 +0530

    changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,7 @@
 import { handlers } from "@/auth"
 export const { GET, POST } = handlers
+
+export async function generateStaticParams() {
+    // Since this is an API route and doesn't require pre-rendering, return an empty array.
+    return [];
+}
